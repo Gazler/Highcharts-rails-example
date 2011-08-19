f4915b72ca9f2d2df1a1cb4c9f05bb6abb137e05
Gary Rennie <webmaster@gazler.com>
2011-08-19 11:09:27 +0100

    Example highcharts app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dashboard::Application.config.secret_token = 'c7821991ca1edf5bc7f47d3f053ffd558108cafd020ca6a9be355a6149d291adfc6a6794c70dbe3bc404de631c0ee9607355fe0ad2eafe61cf18cdb63db29797'
